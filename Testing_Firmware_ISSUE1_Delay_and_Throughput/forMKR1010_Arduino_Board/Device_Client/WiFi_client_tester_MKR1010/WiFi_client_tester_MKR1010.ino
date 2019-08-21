8fac1afeeb5182be72847f69862c16852356e91a
Fan1117 <macfan1995@gmail.com>
2019-08-21 10:01:28 -0400

    add
    Signed-off-by: Fan1117 <macfan1995@gmail.com>

---
+++ Testing_Firmware_ISSUE1_Delay_and_Throughput/forMKR1010_Arduino_Board/Device_Client/WiFi_client_tester_MKR1010/WiFi_client_tester_MKR1010.ino
@@ -0,0 +1,123 @@
+/*
+   Ioannis Smanis
+   For MKR1010 board only
+*/
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <SD.h>
+#include "arduino_secrets.h"
+
+File myFile;
+const uint16_t port = 8000;
+const char * host = "192.168.43.26";
+
+
+int status = WL_IDLE_STATUS;
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+
+
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  // check for the WiFi module:
+  while (!Serial);
+
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Communication with WiFi module failed!");
+  }
+
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.print("Initializing SD card...");
+  
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+  myFile = SD.open("NovaXR.txt", FILE_WRITE);
+  
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("NovaXR testing...");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening NovaXR_test.txt");
+  }
+
+}
+
+void loop()
+{
+  WiFiClient client;
+
+  if (!client.connect(host, port)) {
+
+    Serial.println("Connection to host failed");
+
+    delay(1000);
+    return;
+  }
+
+  Serial.println("Connected to server successful!");
+  int i = 0;
+  String val;
+  unsigned long StartT, timeP1, timeP2, duration = 000;
+
+  //while(1){
+  int packs = 10000;
+  myFile = SD.open("NovaXR.txt", FILE_WRITE);
+
+  //while(1){
+  StartT = micros();
+  for (int a = 0; a < packs; a++) {
+    // send 63 raw bytes
+    client.print("HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHel");
+    myFile.print("HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHel");
+  }
+  timeP1 = micros();
+
+
+  //   Serial.print("10000 Packets in: ");
+  //   Serial.print(timeP1-StartT);
+  //   Serial.print(" Micro Secs");
+  //
+  //   Serial.print(" === Rate: ");
+  //   Serial.print((timeP1-StartT)/packs);
+  //   Serial.print("us/Packet ===>  ");
+  //
+  //   Serial.print(1000000/((timeP1-StartT)/packs));
+  //   Serial.print(" Packets/Sec ");
+  //   Serial.print((1000000/((timeP1-StartT)/packs))*63);
+  //   Serial.println(" Bytes/Sec ");
+  myFile.print("10000 Packets in: ");
+  myFile.print(timeP1 - StartT);
+  myFile.print(" Micro Secs");
+
+  myFile.print(" === Rate: ");
+  myFile.print((timeP1 - StartT) / packs);
+  myFile.print("us/Packet ===>  ");
+
+  myFile.print(1000000 / ((timeP1 - StartT) / packs));
+  myFile.print(" Packets/Sec ");
+  myFile.print((1000000 / ((timeP1 - StartT) / packs)) * 63);
+  myFile.println(" Bytes/Sec ");
+  myFile.close();
+  Serial.println("done.");
+  Serial.println("Disconnecting...");
+  // client.stop();
+  // }
+  delay(10000000);
+}
