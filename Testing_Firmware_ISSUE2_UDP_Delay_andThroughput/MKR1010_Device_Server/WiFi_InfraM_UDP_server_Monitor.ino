dd4f5229efed5b92125801ff527a35408e12928b
modifas <>
2019-09-03 13:45:04 +0300

    New QA firmware - UDP
    New Data transmission  Testing firmware

---
+++ Testing_Firmware_ISSUE2_UDP_Delay_andThroughput/MKR1010_Device_Server/WiFi_InfraM_UDP_server_Monitor.ino
@@ -0,0 +1,131 @@
+/*
+   Author:        Ioannis Smanis
+   Hardware:      NovaXR - MKR1010 board
+   QA Firmware :  UDP Packet Transmission over WiFi
+   Firmware:      Version 0.5
+   QA Test:       ISSUE 02
+   Device Mode:   Infrastructure Mode
+*/
+
+
+// ============= QA Parameters =======================
+#define PACKETSNUM    300  // Number of Packets sent per session
+#define ITERATIONUM   650  // Number of Iterations
+//-- Send Data buffers:
+/*63   bytes*/  //const char Buffer[]="HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello063";  
+/*127  bytes*/  //const char Buffer[]="HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello123";            // a string to send back
+/*505  bytes*/  //const char Buffer[]="HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHel";            // a string to send back
+/*680  bytes*/  //const char Buffer[]="HelloHelloHelloHelloHelloHellHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHello";  
+/*1290 bytes*/    const char Buffer[]="HelloHelloHelloHelloHelloHellHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHello";          
+/*1469 bytes*/  //const char Buffer[]="HelloHelloHelloHelloHelloHellHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHel";          
+// ====================================================
+
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+
+
+
+const char* ssid     = "yourNetworkSSID";
+const char* password = "yourPassword";
+unsigned int UDPPort = 8090;
+WiFiUDP UDP_Server;
+
+
+
+ 
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+  delay(10);
+  
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+  //WiFi.config(IPAddress(192, 168, 3, 60), IPAddress(192, 168, 3, 1), IPAddress(255, 255, 255, 0));
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected"); 
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  UDP_Server.begin(UDPPort);
+  
+  Serial.print("Test Buffer Size: ");
+  Serial.println(sizeof(Buffer));
+  delay(1000); 
+}
+
+
+
+void loop() {
+  uint32_t test1 = micros();
+  //size_t a = handleUDPServer();
+    UDP_Server_Benchmarking();
+   uint32_t test2 = micros();
+}
+
+
+
+
+
+size_t handleUDPServer() {
+
+    size_t dataSize = sizeof(Buffer);
+    UDP_Server.beginPacket(UDP_Server.remoteIP(), UDP_Server.remotePort());
+    //UDP_Server.write(Buffer);
+    UDP_Server.print(Buffer);
+    UDP_Server.endPacket();
+    
+  return dataSize;
+}
+
+
+
+void UDP_Server_Benchmarking(void) {
+   
+  int i = 0;
+  String val;
+  unsigned long StartT, timeP1, timeP2, duration =0;
+  int packs = PACKETSNUM;  //1556
+  int packetSize = sizeof(Buffer);
+
+      //Serial.println("Connected to server successful!");
+      for (i = 0; i < ITERATIONUM; i++) {
+        
+
+          StartT = micros(); // ---------------- Measure Time - Starting Point 
+          
+          for (int a = 0; a < packs; a++) {
+              handleUDPServer();
+           } 
+           
+          timeP1 = micros(); // ---------------- Measure Time - Time Point 1 
+
+          Serial.print(i);
+          Serial.print(": ");
+          Serial.print(packs);
+          Serial.print(" Packets in: ");
+          Serial.print(timeP1 - StartT);
+          Serial.print(" Micro Secs");
+      
+          Serial.print(" === Rate: ");
+          Serial.print((timeP1 - StartT) / packs);
+          Serial.print("us/Packet ===>  ");
+      
+          Serial.print(1000000 / ((timeP1 - StartT) / packs));
+          Serial.print(" Packets/Sec ");
+          Serial.print((1000000 / ((timeP1 - StartT) / packs)) * packetSize);
+          Serial.println(" Bytes/Sec ");
+   
+   }
+}
---
+++ Testing_Firmware_ISSUE2_UDP_Delay_andThroughput/PC_Client/WiFi_UDP_Client.py
@@ -0,0 +1,57 @@
+'''
+Author: Ioannis Smanis
+Project: NovaXR PC UDP_Client  
+Goal: Test the recieving throughput to the Host Side
+The Host Computer that run this script acts as a UDP Client
+'''
+
+
+import socket
+import datetime as dt
+import time
+import sys
+
+
+serverName = '192.168.1.148' # type your device/server IP address
+serverPort = 8090
+
+# Create a UDP socket
+UDP_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Send a Test packet
+message = bytes("Start", "utf-8")
+sent = UDP_client.sendto(message,(serverName,serverPort))
+
+
+# Keep recieving all packets are sent 
+while(1):
+
+	try:
+		startT=time.time()
+		endT=startT +1.0
+		totalSize=0
+		i=0
+		addr=0
+
+		# ---------------------------------------------
+		poinT1=time.time()
+		# -- measure recieved data during 1 second ----
+		while time.time() <= endT:
+			data, server = UDP_client.recvfrom(2048)
+			i=i+1
+			totalSize = totalSize + (len(data)+1)
+		# ---------------------------------------------
+		poinT2=time.time()
+		# ---------------------------------------------
+
+		duration=poinT2-poinT1
+
+		if duration <= 1.09:
+			print("Recieved: ",totalSize, " Bytes/Second " , i, " RX-Buffers")
+
+		totalSize =0
+		i=0
+
+	except KeyboardInterrupt:
+	    UDP_client.close
+	    break
