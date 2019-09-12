cb06697d53bac24b2a44e2148a742e35f8ed3479
“Fan1117” <macfan1995@gmail.com>
2019-09-12 14:22:43 -0400

    orgnize the plot scripts

--- Testing_Firmware_ISSUE2_UDP_Delay_andThroughput/PC_Client/result_plotting.py
+++ Testing_Firmware_ISSUE2_UDP_Delay_andThroughput/PC_Client/result_plotting.py
@@ -1,17 +1,35 @@
+# Packages
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 import os
 import numpy as np
 
+# Data folder structure
 
+# Data
+# -> 1m
+#   -> 63
+#     -> DEVICE.CSV
+#     -> PC.csv
+#   -> 127
+#     -> DEVICE.CSV
+#     -> PC.csv
+# -> 5m
+
+# PC and DEVICE file in each folder
 csv_name_PC = "PC.csv"
 csv_name_DEVICE = "DEVICE.csv"
+
+# All data sets
 col_name_PC = ["iteration","throughput","us_per_buffer"]
 col_name_DEVICE = ["iteration","pack","totaltime","us_per_pack","packs_per_sec","bytes_per_sec",";"]
+
+# Target data sets: Pick desired sets' name from all data set's name above
 target_name_PC = ["iteration","us_per_buffer"]# 1 second / i packets
 target_name_DEVICE = ["iteration","us_per_pack"]
 
+# Plot graphs from Data folder
 def folder_plotting(folder_path):
     for (dirpath, dirnames, filenames) in os.walk(folder_path):
         dirpath = dirpath.replace('\\','/')
@@ -31,37 +49,17 @@ def folder_plotting(folder_path):
             plt.subplot(211)
             plt.title(figure_title + " | Top: PC | Bottom: DEVICE")
             plt.plot(x1, y1)
-            #plt.xticks(np.arange(0,600,100))
-            #plt.yticks(np.arange(min(y1),max(y1)))
             plt.subplot(212)
             plt.plot(x2, y2)
-            #plt.xticks(np.arange(0,600,100))
-            #plt.yticks(np.arange(min(y2),max(y2)))
-            #plt.yticks(np.arange(2000,6000,500))
-            #plt.xticks(np.arange(min(x2), max(x2)+1, 1.0))
-            #plt.yticks(np.arange(min(y2), max(y2)+1, 1.0))
-            #plt.tight_layout()
             plt.savefig(figure_name)
             plt.close()
 
-
+# Load each csv file
 def data_loader(csv_file, col_names, target_names):
     df = pd.read_csv(csv_file, delimiter=";", names= col_names, skiprows=[0,1,3])
-    print(df)
     return pd.to_numeric(df[target_names[0]][2:], downcast='float'), pd.to_numeric(df[target_names[1]][2:], downcast='float')
 
-
-
-# def result_plotting(csv_file, col_names, target_names):
-#     df = pd.read_csv(csv_file, delimiter=";", names= col_names)
-#     #plt.figure(figsize=(20,10))
-#     plt.plot(df[target_names[0]][1:], df[target_names[1]][1:])
-#     #plt.xlabel("Iteration")
-#     #plt.ylabel("PacketDelay: "+ target_names[-1])
-#     # title = csv_file.split("/")[-1].strip(".csv") + ".png"
-#     # plt.savefig(title)
-#     # plt.close()
-
+# Main function
 def main():
     parser = argparse.ArgumentParser ()
     parser.add_argument('--folder', type = str, help = 'The location of the data folder.', default = './Data')
